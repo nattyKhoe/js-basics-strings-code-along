b970e30313550047db1fe54b51662784038f2eae
nattyKhoe <natasha.kusuma@outlook.com>
2022-05-21 22:14:15 +1000

    commit tree 533bb78591330cfedd5454f0684123724bdf5f87

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+let currentUser='Grace Hopper';
+let welcomeMessage=`Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage=welcomeMessage.toUpperCase();
+let shortGreeting=`Welcome, ${currentUser.charAt(0)}!`;
+
